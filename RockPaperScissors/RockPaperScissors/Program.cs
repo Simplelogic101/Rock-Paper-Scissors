14eaeac0b334869cd72496b5942081b1a963a959
7071 111 <7071-111@dadeschools.net>
2018-03-12 12:07:15 -0400

    Intial Project

---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 7071-111
+ * Date: 3/12/2018
+ * Time: 11:54 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace RockPaperScissors
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
