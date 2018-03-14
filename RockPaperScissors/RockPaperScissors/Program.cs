7a87740edf7088e2ba0107f555a18e30dd153de5
7071 111 <7071-111@dadeschools.net>
2018-03-14 12:16:16 -0400

    First version

--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -17,28 +17,89 @@ namespace RockPaperScissors
 			
 			
 			
+			Console.WriteLine("Wellcome to Rock, Paper, Scissors!!!");
+			Console.ReadLine();
+			Console.WriteLine("Make your choice:");
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+			gameFunc();
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
+		
+		public static void gameFunc()
+		{
+         
+			Random Random = new Random();
+			
+			string rock = "ROCK";
+			string paper = "PAPER";
+			string scissor = "SCISSORo";
+		    string choice = Console.ReadLine();
+			
+			int random = Random.Next(1,4);
+			choice = choice.ToUpper();
+			
+			
+			switch (random)
+			{
+			
+				case 1:
+					if (choice == rock) {
+						Console.WriteLine(choice + " <- you vs. us -> " +rock);
+			            Console.WriteLine("It's a Tie.");		
+					}
+					
+					else if (choice == paper) {
+						Console.WriteLine(choice + " <- you vs. us -> " +rock);
+			            Console.WriteLine("It's your defeat.");	
+					
+				    } else if (choice == scissor) {
+						Console.WriteLine(choice + " <- you vs. us -> " +rock);
+			            Console.WriteLine("It's your Victory!!");
+					} else {
+						Console.WriteLine(choice + " is not a choice.");
+					}
+						break;
+				case 2:
+					if (choice == scissor) {
+						Console.WriteLine(choice + " <- you vs. us -> " +paper);
+			            Console.WriteLine("It's your defeat.");		
+		            
+					} else if (choice == paper) {
+						Console.WriteLine(choice + " <- you vs. us -> " + paper);
+			            Console.WriteLine("It's your Victory!!");	
+					
+				    } else if (choice == scissor) {
+						Console.WriteLine(choice + " <- you vs. us -> " +paper);
+			            Console.WriteLine("It's a tie.");
+					} else {
+						Console.WriteLine(choice + " is not a choice.");
+						}
+				        break;
+						
+				case 3:
+					if (choice == rock) {
+						Console.WriteLine(choice + " <- you vs. us -> " +scissor);
+			            Console.WriteLine("It's your defeat.");		
+		            
+					} 
+				    else if (choice == paper) {
+						Console.WriteLine(choice + " <- you vs. us -> " +scissor);
+			            Console.WriteLine("It's your Victory!!");	
+					
+				    } 
+				    else if (choice == scissor) {
+						Console.WriteLine(choice + " <- you vs. us -> " +scissor);
+			            Console.WriteLine("It's a tie.");
+					} 
+				    else {
+						Console.WriteLine(choice + " is not a choice.");
+				        }
+					    break;
+			}
+		
+		}
 	}
 }
+				
